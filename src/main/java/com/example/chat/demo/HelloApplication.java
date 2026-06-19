9ff49baa06fb93b1b619b502f88a2fdce56a090c
I#Oleg <124497826+IOleg-crypto@users.noreply.github.com>
2026-06-19 13:14:33 +0300

    added developer info window and make black design for chat

--- src/main/java/com/example/chat/demo/HelloApplication.java
+++ src/main/java/com/example/chat/demo/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.chat.demo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+
+        Image icon = new Image(getClass().getResourceAsStream("icon/chaticon.jpg"));
+        stage.getIcons().add(icon);
+
+        stage.setTitle("Mercury");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/chat/demo/HelloController.java
+++ src/main/java/com/example/chat/demo/HelloController.java
@@ -2,14 +2,37 @@ package com.example.chat.demo;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Modality; // 1. ДОДАНО ІМПОРТ ДЛЯ MODALITY
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class HelloController {
-
 
     @FXML
     public void onHelloButtonClick(ActionEvent event) {
         System.out.println("I am clicked button");
+    }
+
+    @FXML
+    public void openInfoWindow(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("about-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage aboutStage = new Stage();
+            Image icon = new Image(getClass().getResourceAsStream("icon/chaticon.jpg"));
+            aboutStage.getIcons().add(icon);
+            aboutStage.setTitle("Developer info");
+            aboutStage.setScene(scene);
+            aboutStage.initModality(Modality.APPLICATION_MODAL);
+            aboutStage.setResizable(false);
+            aboutStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error!Cannot open info window.");
+        }
     }
 }
